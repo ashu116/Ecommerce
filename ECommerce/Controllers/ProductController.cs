4ee9c271e0b7e44e05fed0ec17e351ad43bd649d
ashish.hkrnl@gmail.com <ashish.hkrnl@gmail.com>
2023-02-05 17:10:31 +0530

    intialize Ecommerce

---
+++ ECommerce/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Core.Entities;
+using ECommerce.Data;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly storeContext _context;
+        public ProductController(storeContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<List<Product>> GetProducts()
+        {
+            var data = await _context.Products.ToListAsync();
+            return data;
+        }
+        [HttpGet("{id}")]
+        public async Task<Product> GetProduct(int id)
+        {
+            var data=await _context.Products.FindAsync(id);
+            return data;
+        }
+    }
+}
---
+++ Infrastructure/Data/storeContext.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Data
+{
+    public class storeContext:DbContext
+    {
+        public storeContext(DbContextOptions<storeContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
